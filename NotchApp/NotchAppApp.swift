a3fed3eab1e583454a8c3d892ee7d33f5135ab81
Dean Smith <deanmsmith10@gmail.com>
2025-03-14 11:29:37 +0100

    Initial Commit

---
+++ NotchApp/NotchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchAppApp.swift
+//  NotchApp
+//
+//  Created by Dean Smith on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotchAppTests/NotchAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotchAppTests.swift
+//  NotchAppTests
+//
+//  Created by Dean Smith on 14/03/2025.
+//
+
+import Testing
+@testable import NotchApp
+
+struct NotchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
